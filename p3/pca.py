09fa97b95e9393cc56ef82151ffcdd900f589890
Adjective-Object <mhuan13@gmail.com>
2015-03-19 16:49:00 -0400

    THIS IS SOME SHIT

--- p3/pca.py
+++ p3/pca.py
@@ -18,6 +18,7 @@ IMAGE_HEIGHT = 32
 IMAGE_SHAPE = (IMAGE_HEIGHT, IMAGE_WIDTH)
 IMAGE_SIZE = IMAGE_WIDTH * IMAGE_HEIGHT
 
+
 def load_data(actor, category):
     """
     Loads all the data for a given actor under the given category.
@@ -42,11 +43,49 @@ def load_data(actor, category):
             data = imread(path_to_file, True)
             flattened_data = np.ravel(data)
             
-            data_matrix[i, :] = flattened_data
+            data_matrix[i, :] = flattened_data / 255
         except Exception:
             debug("error loading file %s"%(path))
 
     return data_matrix
+
+  
+
+def extend_str(s, l):
+    return s + " " * (l - len(s))
+
+def pretty_table(table, num_format="{:.2f}"):
+    table = [([" "+(item 
+                if isinstance(item, str) 
+                else (num_format.format(item) 
+                        if isinstance(item, float) 
+                        else str(item))) +" " 
+                for item in row]) if row else row
+            for row in table]
+
+    width = len(table[0])
+    row_widths = [max([len(row[i]) for row in table if row]) for i in range(width)]
+       
+    
+    hbar = "-" * sum(row_widths) + "-" * (len(row_widths) -1)
+
+    def make_tabular(t):    
+        return ["|".join(
+                map(lambda tup: 
+                        extend_str(tup[1], row_widths[tup[0]]), 
+                        enumerate(row))
+                if row else [hbar]
+                ) for row in t]
+    
+    
+    return (" " + hbar + " " + "\n|" +
+            "|\n|".join(
+                make_tabular(table)
+            ) + "|\n" +" " + hbar + " ")
+    
+def most_common(lst):
+    return max(set(lst), key=lst.count) if len(lst) > 0 else None
+
 
 
 def pca(X):
@@ -81,6 +120,8 @@ def pca(X):
     # return the projection matrix, the variance and the mean
     return V,S,mean_X
 
+
+
 def unflatten_face(flattened_face):
     ''' reshape a (1024) vector into a (32,32) image
     '''
@@ -107,6 +148,7 @@ def showall_flattened(imgs_flat):
     ''' reshape a set of (1024) vectors into (32,32) images, and display all of
         them in a greyscale grid
     '''
+    print(imgs_flat)
     s=imgs_flat.shape
     unflattened = np.reshape(imgs_flat, (s[0],32,32))
     debug(unflattened.shape)
@@ -126,6 +168,8 @@ def ssd(img1, img2):
         (euclidian distance) between the images
     '''
     return np.sum((img1 - img2) ** 2 )
+
+
 
 def project_to_space(vec, bases):
     return np.dot(bases, vec)
@@ -186,6 +230,13 @@ def closest_projections(test_faces, training_faces, bases, k):
         test_faces
     )))
     
+    debug(projected_test_faces)
+    debug(bases[:k])    
+    
+    reconstructed_test_faces = np.dot(projected_test_faces, bases[:k])
+    
+    showall_flattened(reconstructed_test_faces)
+    
     # plt.figure()
     # plt.plot(projected_training_faces[:,0], projected_training_faces[:,1], 'ro')
     # plt.plot(projected_test_faces[:,0], projected_test_faces[:,1], 'go')
@@ -193,42 +244,7 @@ def closest_projections(test_faces, training_faces, bases, k):
     
     return [closest_face(face, bases[:k], projected_training_faces)
                 for face in test_faces]
-        
-def extend_str(s, l):
-    return s + " " * (l - len(s))
-
-def pretty_table(table, num_format="{:.2f}"):
-    table = [([" "+(item 
-                if isinstance(item, str) 
-                else (num_format.format(item) 
-                        if isinstance(item, float) 
-                        else str(item))) +" " 
-                for item in row]) if row else row
-            for row in table]
-
-    width = len(table[0])
-    row_widths = [max([len(row[i]) for row in table if row]) for i in range(width)]
-       
-    
-    hbar = "-" * sum(row_widths) + "-" * (len(row_widths) -1)
-
-    def make_tabular(t):    
-        return ["|".join(
-                map(lambda tup: 
-                        extend_str(tup[1], row_widths[tup[0]]), 
-                        enumerate(row))
-                if row else [hbar]
-                ) for row in t]
-    
-    
-    return (" " + hbar + " " + "\n|" +
-            "|\n|".join(
-                make_tabular(table)
-            ) + "|\n" +" " + hbar + " ")
-    
-def most_common(lst):
-    return max(set(lst), key=lst.count)
-
+      
 # the "main function" for testing
 def do_test():
     font = {'family' : 'normal',
@@ -239,12 +255,12 @@ def do_test():
     actor_dirnames = [
                       "Adam_Sandler"
                      ,"Andrea_Anders" 
-                     #,"Dianna_Agron"
+                     ,"Dianna_Agron"
                      ,"Ashley_Benson"
                      ,"Adrien_Brody"
-                     #,"Christina_Applegate"
+                     ,"Christina_Applegate"
                      ,"Gillian_Anderson"
-                     #,"Aaron_Eckhart"
+                     ,"Aaron_Eckhart"
                       ]
     random.shuffle(actor_dirnames)
     debug("actors: %s"%(", ".join(actor_dirnames)))
@@ -253,6 +269,8 @@ def do_test():
     data = [load_data(actor, "training") for actor in actor_dirnames]
     datalens = list(map(len, data))
     all_actor_faces = np.concatenate(data, axis=0)
+
+    # debug(data[0])
     
     def get_name_from_ind(ind):
         dataind = 0
@@ -263,26 +281,36 @@ def do_test():
     
     debug("performing pca on all actors")
     components, single_values, avg_face = pca(all_actor_faces)
-    #showall_flattened(components[0:25])
-    ''''''
+    # showall_flattened(np.concatenate(([avg_face], components[0:24]), axis=0))
     
     emptys = np.zeros((32,32))
     fulls =  np.empty((32,32))
     fulls.fill(255)
     max_dist = ssd(emptys, fulls)    
     
+    
     results_table = [["k", "Name", "Accuracy"]]
     similarity_table = [["k", "actual", "percieved", "count"]]
     
+    
     complete_num = 0
     complete_num_correct = 0
-    for k in [2, 5, 10, 20, 50, 80, 100, 150, 200]:
+    for k in [100]:
         results_table.append(None)
         similarity_table.append(None)
         total_num_correct, total_num = 0, 0
         # results_table.append([k])
         for name in actor_dirnames:
             valid_faces = load_data(name, "validation")
+            mean_faces = np.repeat(
+                            np.array([avg_face]), 
+                            valid_faces.shape[0],
+                            axis=0)
+            #valid_faces = np.subtract(valid_faces, mean_faces)
+            #debug("vald:", valid_faces.shape)
+            #debug("mean:", mean_faces.shape)
+            #debug("diff:", valid_faces.shape)
+            
             total_num += len(valid_faces)
             misses = []
             
@@ -319,7 +347,6 @@ def do_test():
     
     debug(pretty_table(results_table))
     debug(pretty_table(similarity_table))
-    
     
 
 if __name__ == "__main__":
