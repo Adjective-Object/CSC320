250d314a2ef97d8b70f72b7058806659d9df14f6
Adjective-Object <mhuan13@gmail.com>
2015-03-19 13:08:10 -0400

    i did

---
+++ p3/p3.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 19 10:17:29 2015
+
+@author: g4rbage
+"""
+
+import sys
+from pca import *
+
+DEBUG = True
+def debug(*args):
+    if DEBUG:
+        # using sys.stdout to make it work independent of python version
+        sys.stdout.write(" ".join([str(i) for i in args]))
+        sys.stdout.write("\n")
+        sys.stdout.flush()
+
+if __name__ == "__main__":
+    do_test()
